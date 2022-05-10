210845c3ce64ff669435e4c233e868abe29d0a48
StenV7 <0320484@student.rocvantwente.nl>
2022-05-10 12:38:52 +0200

    database migration voorbereiding

---
+++ F1Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using F1Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace F1Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Country> Country { get; set; }
+        public DbSet<Circuit> Circuit { get; set; }
+        public DbSet<Team> Team { get; set; }
+        public DbSet<Grandprix> Grandprix { get; set; }
+        public DbSet<Result> Result { get; set; }
+        public DbSet<Driver> Driver { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    }
+}
