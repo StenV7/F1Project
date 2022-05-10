7ff70dbb90df8e4c280443815719e45dff4d7d7c
StenV7 <0320484@student.rocvantwente.nl>
2022-05-10 11:45:50 +0200

    rework

--- F1Project/Models/circuit.cs
+++ F1Project/Models/circuit.cs
@@ -6,14 +6,15 @@ namespace F1Project.Models
     {
         public int ID { get; set; }
         [StringLength(50,ErrorMessage ="Macimumlengte van {0} is {1} tekens")]
-        public string Name { get; set; } = String.Empty;
-        public string? Description { get; set; } = String.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
         public double? Latitute { get; set; } 
         public double? Longitute { get; set; }
         [DataType(DataType.Url)]
         [StringLength(250, ErrorMessage = "MaximumLengte van {0} is {1} tekens")]
-        public string? wiki { get; set; } = String.Empty;
-        public IEnumerable<Result> Races { get; set; } = Enumerable.Empty<Results>();
+        public string? Wiki { get; set; } = string.Empty;
+        public Country? Country { get; set; }
+        public IEnumerable<Result> Races { get; set; } = Enumerable.Empty<Result>();
 
 
     }
