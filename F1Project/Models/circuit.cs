0a9b026386058c691e066c9d78edec9ed07e6e17
clint ellenbroek <0322399@student.rocvantwente.nl>
2022-05-10 11:34:21 +0200

    Circuit

---
+++ F1Project/Models/circuit.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace F1Project.Models
+{
+    public class Circuit
+    {
+        public int ID { get; set; }
+        [StringLength(50,ErrorMessage ="Macimumlengte van {0} is {1} tekens")]
+        public string Name { get; set; } = String.Empty;
+        public string? Description { get; set; } = String.Empty;
+        public double? Latitute { get; set; } 
+        public double? Longitute { get; set; }
+        [DataType(DataType.Url)]
+        [StringLength(250, ErrorMessage = "MaximumLengte van {0} is {1} tekens")]
+        public string? wiki { get; set; } = String.Empty;
+        public IEnumerable<Result> Races { get; set; } = Enumerable.Empty<Results>();
+
+
+    }
+}
