761cf9c017a2c18af82d24d2fbee46cc2c32f1c1
StenV7 <0320484@student.rocvantwente.nl>
2022-05-10 11:58:42 +0200

    CIRCUIT

--- F1Project/Models/Circuit.cs
+++ F1Project/Models/Circuit.cs
@@ -5,10 +5,10 @@ namespace F1Project.Models
     public class Circuit
     {
         public int ID { get; set; }
-        [StringLength(50,ErrorMessage ="Macimumlengte van {0} is {1} tekens")]
+        [StringLength(50, ErrorMessage = "Macimumlengte van {0} is {1} tekens")]
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
-        public double? Latitute { get; set; } 
+        public double? Latitute { get; set; }
         public double? Longitute { get; set; }
         [DataType(DataType.Url)]
         [StringLength(250, ErrorMessage = "MaximumLengte van {0} is {1} tekens")]
