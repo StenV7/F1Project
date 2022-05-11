07f3707b7c1fa908863e811c46f40a778b776a78
StenV7 <0320484@student.rocvantwente.nl>
2022-05-11 09:46:17 +0200

    Result and Circuits controller

---
+++ F1Project/Controllers/CircuitController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using F1Project.Data;
+using F1Project.Models;
+
+namespace F1Project.Controllers
+{
+    public class CircuitController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CircuitController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Circuit
+        public async Task<IActionResult> Index()
+        {
+              return _context.Circuits != null ? 
+                          View(await _context.Circuits.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Circuits'  is null.");
+        }
+
+        // GET: Circuit/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Circuits == null)
+            {
+                return NotFound();
+            }
+
+            var circuit = await _context.Circuits
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (circuit == null)
+            {
+                return NotFound();
+            }
+
+            return View(circuit);
+        }
+
+        // GET: Circuit/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Circuit/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Description,Latitute,Longitute,Wiki")] Circuit circuit)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(circuit);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(circuit);
+        }
+
+        // GET: Circuit/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Circuits == null)
+            {
+                return NotFound();
+            }
+
+            var circuit = await _context.Circuits.FindAsync(id);
+            if (circuit == null)
+            {
+                return NotFound();
+            }
+            return View(circuit);
+        }
+
+        // POST: Circuit/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Description,Latitute,Longitute,Wiki")] Circuit circuit)
+        {
+            if (id != circuit.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(circuit);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CircuitExists(circuit.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(circuit);
+        }
+
+        // GET: Circuit/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Circuits == null)
+            {
+                return NotFound();
+            }
+
+            var circuit = await _context.Circuits
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (circuit == null)
+            {
+                return NotFound();
+            }
+
+            return View(circuit);
+        }
+
+        // POST: Circuit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Circuits == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Circuits'  is null.");
+            }
+            var circuit = await _context.Circuits.FindAsync(id);
+            if (circuit != null)
+            {
+                _context.Circuits.Remove(circuit);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CircuitExists(int id)
+        {
+          return (_context.Circuits?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ F1Project/Controllers/ResultController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using F1Project.Data;
+using F1Project.Models;
+
+namespace F1Project.Controllers
+{
+    public class ResultController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResultController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Result
+        public async Task<IActionResult> Index()
+        {
+              return _context.Results != null ? 
+                          View(await _context.Results.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Results'  is null.");
+        }
+
+        // GET: Result/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Results == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _context.Results
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return View(result);
+        }
+
+        // GET: Result/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Result/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Year,Racenumber,Date,Rounds,Time")] Result result)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(result);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(result);
+        }
+
+        // GET: Result/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Results == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _context.Results.FindAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return View(result);
+        }
+
+        // POST: Result/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Year,Racenumber,Date,Rounds,Time")] Result result)
+        {
+            if (id != result.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(result);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResultExists(result.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(result);
+        }
+
+        // GET: Result/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Results == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _context.Results
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return View(result);
+        }
+
+        // POST: Result/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Results == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Results'  is null.");
+            }
+            var result = await _context.Results.FindAsync(id);
+            if (result != null)
+            {
+                _context.Results.Remove(result);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResultExists(int id)
+        {
+          return (_context.Results?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
